de19bb209c7e3f7cb0cfde228a104fd010873b62
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-08-01 16:32:53 -0400

    Add post request tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,21 @@ def test_index_get():
     
     assert response.status_code == 200
 
-def test_index_post():
-    response = client.post("/")
+def test_index_post_data():
+    response = client.post("/", data={"data": "test_data"})
     
     assert response.status_code == 200
+    assert "test_data" in response.text
+
+def test_index_post_cap_string():
+    response = client.post("/", data={"cap_string": "test_cap_string"})
+    
+    assert response.status_code == 200
+    assert "test_cap_string" in response.text
+
+def test_index_post_data_and_cap_string():
+    response = client.post("/", data={"data": "test_data", "cap_string": "test_cap_string"})
+    
+    assert response.status_code == 200
+    assert "test_data" in response.text
+    assert "test_cap_string" in response.text
