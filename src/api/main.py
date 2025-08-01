0eb610c63fd02fe2459504c9b934f0e5d52f4c53
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-08-01 16:28:48 -0400

    Display input of both forms on submit

--- src/api/main.py
+++ src/api/main.py
@@ -7,11 +7,11 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.api_route("/", methods=["GET", "POST"], response_class=HTMLResponse)
-def index(request: Request, input: str = Form(None), cap_string: str = Form(None)):
+def index(request: Request, data: str = Form(None), cap_string: str = Form(None)):
     """
     Load index.html. If a post request was sent and data has been returned, include the data in the response context.
     """
     if request.method == "GET":
         return templates.TemplateResponse(request, "index.html")
     else:
-        return templates.TemplateResponse(request, "index.html", {"input": input, "cap_string": cap_string})
+        return templates.TemplateResponse(request, "index.html", {"data": data, "cap_string": cap_string})
